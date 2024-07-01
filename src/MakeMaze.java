8a279248b802b31bd8c9f9928f9ef0c905f74547
riku0817 <inbederacade@gmail.com>
2024-07-01 09:49:35 +0900

    壁を生成するサンプルコードを追加

--- src/MakeMaze.java
+++ src/MakeMaze.java
@@ -1,18 +1,18 @@
+import java.util.Random;
+import java.util.Vector;
 
 public class MakeMaze extends MyFrame {
 	public void run() {
+		boolean testloop = true;
+
 		MazeWall[][] mazewall = new MazeWall[15][15];
-		while (true) {
-			boolean isStartSearched = false;
+		Vector<MazeStart> mazestart = new Vector<MazeStart>();
+		while (testloop == true) {
 			for (int i = 0; i < 15; i++) {
 
 				for (int j = 0; j < 15; j++) {
 
-					if (Math.random() < 0.5) {
-						mazewall[i][j] = new MazeWall(1); // 列
-					} else {
-						mazewall[i][j] = new MazeWall(0);
-					}
+					mazewall[i][j] = new MazeWall(0);
 
 					if (j == 0 || j == 14 || i == 0 || i == 14) {
 						mazewall[i][j] = new MazeWall(1); // 外壁
@@ -21,23 +21,28 @@ public class MakeMaze extends MyFrame {
 					if (mazewall[i][j].isWall == 1) {
 						drawString("■", 30 + (i * 16), 80 + (j * 16), 20);
 					}
-					if ((i + 1) % 2 == 0 && (j + 1) % 2 == 0) {
+					if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0) {
+						mazestart.add(new MazeStart(i, j));
 						System.out.print(i);
 						System.out.print(",");
 						System.out.print(j);
 						System.out.print(" ");
 					}
-					if (i == 14 && j == 14) {
-						isStartSearched = true;
-					}
 
-					sleep(0.01);
 				}
 
 			}
-			System.out.print("\n" + isStartSearched);
-			sleep(0.1);
-			clear();
+			Random rand = new Random();
+			int num = rand.nextInt(mazestart.size()); // xyとも偶数のスタート位置を変数numに代入
+			System.out.println("\n" + mazestart.get(num).x);
+			System.out.println("\n" + mazestart.get(num).y);
+			testloop = false;
+
+			// 壁を作るテストコード------------------
+			drawString("■", 30 + (mazestart.get(num).x * 16), 80 + (mazestart.get(num).y * 16), 20);
+			mazewall[mazestart.get(num).x][mazestart.get(num).y].isWall = 1;
+			System.out.println(mazewall[mazestart.get(num).x][mazestart.get(num).y].isWall);
+			//---------------------------------------
 		}
 	}
 }
---
+++ src/MazeStart.java
@@ -0,0 +1,10 @@
+
+public class MazeStart {
+	int x;// i
+	int y;// j
+
+	public MazeStart(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
